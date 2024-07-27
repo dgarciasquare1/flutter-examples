c8e1dc855276c4acb4466c59907bfde5f39ce092
David Garcia Ruiz <david.garcia@square1.io>
2024-07-28 00:58:48 +0200

    adv basics

---
+++ adv_basics/lib/questions_summary/questions_summary.dart
@@ -0,0 +1,27 @@
+import 'package:adv_basics/questions_summary/summary_item.dart';
+import 'package:flutter/material.dart';
+
+class QuestionsSummary extends StatelessWidget {
+  const QuestionsSummary(this.summaryData, {super.key});
+
+  final List<Map<String, Object>> summaryData;
+
+  bool isRightAnswer(Map<String, Object> data) =>
+      data['correct_answer'] == data['user_answer'];
+
+  @override
+  Widget build(Object context) {
+    return SizedBox(
+      height: 400,
+      child: SingleChildScrollView(
+        child: Column(
+          children: summaryData.map(
+            (data) {
+              return SummaryItem(data);
+            },
+          ).toList(),
+        ),
+      ),
+    );
+  }
+}
